6a3ece8e8725a61c5e3600266eb140550e3188e0
Mattias Harrysson <mathar@kth.se>
2016-02-14 15:25:55 +0100

    combine mlp and cnn into one file

--- net/train.lua
+++ net/train.lua
@@ -6,55 +6,72 @@ require "torch"
 require "optim"
 require "xlua"
 
-mnist = require "mnist"
-
-print("nn: prepare training dataset")
-
-mnist_train = mnist.traindataset()
-x_train = mnist_train.data / 255
-y_train = mnist_train.label
-
-print("nn: prepare test dataset")
-
-mnist_test = mnist.testdataset()
-x_test = mnist_test.data / 255
-y_test = mnist_test.label
+cmd = torch.CmdLine()
+cmd:text("Train a MNIST classifier")
+cmd:text()
+cmd:text("Options")
+cmd:option("-model", "cnn", "Model type, cnn or mlp")
+cmd:option("-eta", 0.1, "Learning rate")
+cmd:option("-bsize", 5, "Batch size")
+cmd:option("-epochs", 4, "Training epochs")
+args = cmd:parse(arg)
 
 print("nn: prepare model")
 
 classes = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} -- 10 is the label for 0
 
-model = nn.Sequential()
-model:add(nn.Reshape(1, 28, 28))
--- 1
-model:add(nn.SpatialConvolution(1, 16, 5, 5))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(3, 3))
--- 2
-model:add(nn.SpatialConvolution(16, 64, 3, 3))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(2, 2))
--- 3
-model:add(nn.Reshape(64 * 3 * 3))
-model:add(nn.Linear(64 * 3 * 3, 100))
-model:add(nn.ReLU())
-model:add(nn.Linear(100, #classes))
+if args.model == "cnn" then
+    model = nn.Sequential()
+    model:add(nn.Reshape(1, 28, 28))
+    -- 1
+    model:add(nn.SpatialConvolution(1, 16, 5, 5))
+    model:add(nn.ReLU())
+    model:add(nn.SpatialMaxPooling(3, 3))
+    -- 2
+    model:add(nn.SpatialConvolution(16, 64, 3, 3))
+    model:add(nn.ReLU())
+    model:add(nn.SpatialMaxPooling(2, 2))
+    -- 3
+    model:add(nn.Reshape(64 * 3 * 3))
+    model:add(nn.Linear(64 * 3 * 3, 100))
+    model:add(nn.ReLU())
+    model:add(nn.Linear(100, #classes))
+elseif args.model == "mlp" then
+    inputs = 28 * 28
+    hidden = 100
+    outputs = #classes
+
+    model = nn.Sequential()
+    model:add(nn.Reshape(inputs))
+    model:add(nn.Linear(inputs, hidden))
+    model:add(nn.ReLU())
+    model:add(nn.Linear(hidden, outputs))
+else
+    error("nn: unknown model name \"" .. args.model .. "\"")
+end
 
 criterion = nn.CrossEntropyCriterion() -- LogSoftMax with ClassNLLCriterion
 
 params, grad_params = model:getParameters()
-optim_config = {
-    learningRate = 0.1,
-}
+optim_config = { learningRate = args.eta }
+epochs = args.epochs
+batch_size = args.bsize
 
-epochs = 4
-batch_size = 5
-batch_index = 0
+print("nn: prepare training and testing datasets")
 
+mnist = require "mnist"
+
+mnist_train = mnist.traindataset()
+x_train = mnist_train.data / 255
+y_train = mnist_train.label
 train_size = x_train:size()[1]
+
+mnist_test = mnist.testdataset()
+x_test = mnist_test.data / 255
+y_test = mnist_test.label
 test_size = x_test:size()[1]
 
-print("nn: train model")
+print("nn: train model: " .. args.model)
 
 function feval(x)
     if x ~= params then
@@ -93,6 +110,7 @@ end
 
 iterations = epochs * math.ceil(train_size / batch_size)
 updates = math.ceil(0.1 * iterations)
+batch_index = 0
 for i = 1, iterations do
     optim.adagrad(feval, params, optim_config)
     if i % updates == 0 then
@@ -104,8 +122,11 @@ for i = 1, iterations do
     xlua.progress(i, iterations)
 end
 
+model_name = string.format(
+    "%s_model-%s.t7",
+    args.model,
+    os.date("%y%m%d_%H%M%S")
+)
 print("nn: save model")
-
-name = "cnn_model.t7"
-netToolkit.saveNet(name, model)
-print("nn: model saved as " .. name)
+netToolkit.saveNet(model_name, model)
+print("nn: model saved as " .. model_name)
